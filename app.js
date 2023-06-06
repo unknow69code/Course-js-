4045571294332030c594024b9b5f8fc8311b94a6
unknow69code <anonymus69007@gmail.com>
2023-06-06 17:58:53 -0500

    Course basic js

---
+++ app.js
@@ -0,0 +1,100 @@
+// // document.write("<h1>Hello World</h1>")
+// //TYPE DATE
+// "Hello World" //String
+// 'Hello World' //String
+
+// 102010201     //Numbers
+// 10.3          //Numbers
+
+// false         //Boolean
+// true
+
+//               //Array
+// ["jose", "juan", "Ricard0"]
+// [1, 123, 443]
+// [true, false, true, false]
+
+//             //Objects
+
+//     // {
+//         "username": "Jose",
+//         "score": 102,
+//         "hours": 45,
+//         "profesional": true,
+//         "friends": ["Juan", "jauna"]
+//     }
+
+//Print console
+// console.log( "Error " + 200)
+// console.log([1, 2, 3, 4, 5])
+
+//stores user and las names
+// var { nameUser, lastName } = getUserName()
+
+// Print Users and lasName
+// console.log(nameUser + lastName)
+
+//funtion getuser
+// function getUserName() {
+//     var nameUser = "Jose manuel"
+//     let lastName = "Mendoza V"
+//     return { nameUser, lastName }
+//   }
+
+// var user = "jose123";
+// var passw = "as";
+
+// var result = user == passw;
+
+// if (result === true) {
+//   alert('correct');
+// } else {
+//     alert('wrong');
+// }
+
+//Flujo de datos autenticacion y manipulacion
+// let typeCard = "Fallabella card";
+
+// switch (typeCard) {
+//   case "Debid card":
+//     console.log("This is a card debid");
+//   case "Credit card":
+//     console.log("This is a card Credit");
+//     break;
+//   case "Fallabella card":
+//     console.log("This is a Fallabella card");
+//     break;
+//   default:
+//     console.log("this section does not exist.");
+//     break;
+// }
+
+//Ciclos repetitivos while
+// var count = 50;
+// while (count > 0) {
+//   console.log("Hello world");
+//   count--
+// }
+
+// var count = 0;
+// while (count <= 50) {
+//   console.log(count);
+//   count++
+// }
+
+// //Ciclo for recorriendo elementos en un array
+// var nameUser = ["Juan", "Jose", "Camila"]
+
+// for (let index = nameUser.length - 1; index >= 0 ;index--) {
+//   console.log(nameUser[index])
+//   document.write(nameUser[index])
+// }
+
+//FUNCIONES
+
+// function greeting(name) {
+//   console.log("Hello compas: " + name)
+// } 
+
+// greeting("Jose Manuel Mendoza")
+// greeting("Jose David Ricardo")
